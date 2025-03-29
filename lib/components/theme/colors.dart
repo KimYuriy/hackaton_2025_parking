e4d1aa56beffc6ef90f386338c881a480d93db93
1eG0ist <diadypetya@yandex.ru>
2025-03-29 16:06:26 +0300

    add theme

---
+++ lib/components/theme/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class AppColors {
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+  static const grey = Color(0xFFd7d7d7);
+  static const cream = Color(0xFFd3c7c3);
+  static const beige = Color(0xFF7d6860);
+  static const plum = Color(0xFF4d4a4f);
+  static const charcoal = Color(0xFF191512); // Color(0xFF1C2F1);
+
+  const AppColors();
+}

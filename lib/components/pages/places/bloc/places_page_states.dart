ec0567b834b98282e829a8cd3bc6dda1a5ae3005
KimYuriy <71920431+KimYuriy@users.noreply.github.com>
2025-03-29 22:21:33 +0300

    Delete lib/components/pages/places/bloc/places_page_states.dart

--- lib/components/pages/places/bloc/places_page_states.dart
+++
@@ -1,8 +0,0 @@
-part of 'places_page_bloc.dart';
-
-@freezed
-class PlacesPageStates with _$PlacesPageStates {
-  const factory PlacesPageStates.loading() = _Loading;
-  const factory PlacesPageStates.dataLoaded({ required List<ParkingPlaceModel> allPlaces }) = _DataLoaded;
-  const factory PlacesPageStates.errorLoading() = _ErrorLoading;
-}
